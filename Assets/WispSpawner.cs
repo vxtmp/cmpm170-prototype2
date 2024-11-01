31cb24ca5d5e04b91bb937336f253ffb59fb0969
norm z <115136784+vxtmp@users.noreply.github.com>
2024-11-01 11:58:25 -0700

    wispspawner

---
+++ Assets/WispSpawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispSpawner : MonoBehaviour
+{
+    public class WispPool : MonoBehaviour
+    {
+        public static WispPool Instance { get; private set; }
+
+        [SerializeField] private GameObject wispPrefab;
+        [SerializeField] private int initialPoolSize = 20;
+
+        private Queue<GameObject> wispPool = new Queue<GameObject>();
+
+        private void Awake()
+        {
+            // Singleton setup
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            // Prepopulate pool
+            for (int i = 0; i < initialPoolSize; i++)
+            {
+                GameObject wisp = Instantiate(wispPrefab);
+                wisp.SetActive(false);
+                wispPool.Enqueue(wisp);
+            }
+        }
+
+        public GameObject GetWisp()
+        {
+            // Reuse wisp from pool or create a new one if pool is empty
+            if (wispPool.Count > 0)
+            {
+                GameObject wisp = wispPool.Dequeue();
+                wisp.SetActive(true);
+                return wisp;
+            }
+            else
+            {
+                GameObject newWisp = Instantiate(wispPrefab);
+                return newWisp;
+            }
+        }
+
+        public void ReturnWisp(GameObject wisp)
+        {
+            wisp.SetActive(false);
+            wispPool.Enqueue(wisp);
+        }
+    }
+}
