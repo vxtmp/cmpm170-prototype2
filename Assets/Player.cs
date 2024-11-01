772483cbeb1f19fe6dddeb0cf073907419da4470
norm z <115136784+vxtmp@users.noreply.github.com>
2024-10-31 17:47:47 -0700

    slight update to wisp / player scripts

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float chargeDuration = 0.0f;
+    private float chargeSpeed = 10.0f;
+    public const float MAX_CHARGE_DURATION = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +19,45 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    void startCharging()
+    {
+
+    }
+    void pounceForward()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 0.1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -0.1f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+        // while mouse held down, charge up
+        if (Input.GetMouseButton(0))
+        {
+            startCharging();
+        }
+        // when mouse released, pounce forward
+        if (Input.GetMouseButtonUp(0))
+        {
+            pounceForward();
+        }
+    }
+
+    // on trigger enter. check if the object has the tag "Enemy"
+    // if it does, 
 }
